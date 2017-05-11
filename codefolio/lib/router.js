e24db778643fe7b3900686fc9599be8ae1465592
nsrchemie <nsrchemie@gmail.com>
2017-05-10 21:40:35 -0400

    added new routes and fixed templating in navbar links

--- codefolio/lib/router.js
+++ codefolio/lib/router.js
@@ -7,4 +7,14 @@ Router.map(function() {
 		path:'/',
 		template:'home'
 	});
+	this.route('about', {
+		path:'/about',
+		template:'about'
+	});
+	this.route('work');
+	this.route('blog', {
+		path:'/blog',
+		template:'blog'
+	});
+	this.route('contact');
 });
