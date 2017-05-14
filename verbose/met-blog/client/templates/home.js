9958e206fadf3daa5e3418fc0af51acf02a81f1c
nsrchemie <nsrchemie@gmail.com>
2017-05-13 21:46:24 -0400

    added simple console.log based template helper callbacks

---
+++ verbose/met-blog/client/templates/home.js
@@ -0,0 +1,12 @@
+Template.home.created = () => {
+  console.log('Created home template');
+};
+
+Template.home.rendered = () => {
+  console.log('Rendered home template');
+};
+
+
+Template.home.destroyed = () => {
+  console.log('Destroyed home template');
+};
