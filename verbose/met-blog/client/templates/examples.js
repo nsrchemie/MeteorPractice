183d5f02cc4a6fdf64fcf8fd91d11d18f3c6d07b
nsrchemie <nsrchemie@gmail.com>
2017-05-18 16:05:33 -0400

    working with #each block helper and template helpers to add static blog entries

--- verbose/met-blog/client/templates/examples.js
+++ verbose/met-blog/client/templates/examples.js
@@ -1,17 +1,17 @@
-Template.contextExample.rendered = function() {
-  console.log('Rendered Context Example', this.data);
-};
+// Template.contextExample.rendered = function() {
+//   console.log('Rendered Context Example', this.data);
+// };
 
-Template.contextExample.helpers({
- logContext: function() {
-   console.log('Context Log Helper', this);
+// Template.contextExample.helpers({
+//  logContext: function() {
+//    console.log('Context Log Helper', this);
 
-  return Session.get('randomNumber');
-}
-});
+//   return Session.get('randomNumber');
+// }
+// });
 
-Template.contextExample.events({
-   'click button': function(e,template) {
-     Session.set('randomNumber', Math.random(0,99));
-  }
-});
+// Template.contextExample.events({
+//    'click button': function(e,template) {
+//      Session.set('randomNumber', Math.random(0,99));
+//   }
+// });
--- verbose/met-blog/client/templates/home.js
+++ verbose/met-blog/client/templates/home.js
@@ -21,6 +21,22 @@ Template.home.helpers({
     someText: 'This text was set using a helper of the parent template',
     someNested: {
       text: 'That comes from someNested.text'
-}};}
-
+}}},
+  
+  postsList: () => {
+    return [
+      {
+        title: 'My Second entry',
+        description: 'Borem sodum color sit amet, consetetur sadipscing elitr.',
+        author: 'Fabian Vogelsteller',
+        timeCreated: moment().subtract(3, 'days').unix()
+      },
+      {
+        title: 'My First entry',
+        description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+        author: 'Fabian Vogelsteller',
+        timeCreated: moment().subtract(7, 'days').unix()
+      }
+    ];
+  }
 });
