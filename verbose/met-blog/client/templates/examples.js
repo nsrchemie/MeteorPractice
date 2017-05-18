3bea34ae62b07f5a16f77884c28bda71e1cd2b9a
nsrchemie <nsrchemie@gmail.com>
2017-05-17 21:17:18 -0400

    used session package with template helper to create event render response (random number display on button click in this case)

--- verbose/met-blog/client/templates/examples.js
+++ verbose/met-blog/client/templates/examples.js
@@ -5,5 +5,13 @@ Template.contextExample.rendered = function() {
 Template.contextExample.helpers({
  logContext: function() {
    console.log('Context Log Helper', this);
+
+  return Session.get('randomNumber');
+}
+});
+
+Template.contextExample.events({
+   'click button': function(e,template) {
+     Session.set('randomNumber', Math.random(0,99));
   }
 });
